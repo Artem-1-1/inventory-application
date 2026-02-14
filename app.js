6e5c6a27f076e490cf5ea14f3f19204e0a79112f
Artem-1-1 <aleksejchukartem@gmail.com>
2026-02-14 12:02:52 +0300

    Added 404 page for app.

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import 'dotenv/config';
 import express from "express";
 import indexRouter from "./routes/indexRouter.js"
 import productsRouter from "./routes/productsRouter.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
-app.set('view engine', 'ejs');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
